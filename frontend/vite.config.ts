e134c221ce86c03480a98465b19a6e4257a8c158
MZohry <mzohry@gmail.com>
2024-12-25 15:02:55 +0200

    Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    open: false,
+    host: true,
+  },
   plugins: [react()],
 })
